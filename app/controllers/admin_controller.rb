ad9bb61587b7d026481599c714ceccd4893e5e01
Joshua Siler <joshua.siler@gmail.com>
2009-06-12 08:56:07 -0700

    Bug fixes to assignment logic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -32,13 +32,18 @@ class AdminController < ApplicationController
     # processes requests and sets calendar
     enrollment = Enrollment.get_latest()
     
+	  # clear out all previously assigned shifts
+	  # facilitates reruns
+	  Requestedshift.connection.execute("update requestedshifts set assigned_flag = 0") # where target_date = '#{datectrl.to_s}'")
+
     # arbitrary number for first pass through loop
     unclaimed_shifts = 10
     # one less than the number of shifts for first pass   
     max_shifts = 0
     # monitor to check if unclaimed stops improving with multiple passes
     last_unclaimed_shifts = 0
-    
+		assignedcnts = {}    
+		altcnts = {}
     while unclaimed_shifts > 0 && last_unclaimed_shifts != unclaimed_shifts
       reqs = Request.get_priorities(enrollment.id)
       
@@ -57,42 +62,43 @@ class AdminController < ApplicationController
       while datectrl < dateend
 	
 	#Tuesdays and Thursdays only
-	if datectrl.wday == 2 || datectrl.wday == 4
-	  # clear out all previously assigned shifts for this request on this day
-	  # facilitates reruns
-	  Requestedshift.connection.execute("update requestedshifts set assigned_flag = 0 where target_date = '#{datectrl.to_s}'")
+	if (datectrl.wday == 2 || datectrl.wday == 4) && !(assignedcnts[datectrl] == ConfigValue.find_by_key('max_assigned').value.to_i &&  altcnts[datectrl] == ConfigValue.find_by_key('max_alts').value.to_i)
 	  # for each day in the range
-	  assigned = 0;
-	  alts = 0;
-	  
+		if assignedcnts[datectrl].nil?
+			assignedcnts[datectrl] = 0
+		end
+	 	if altcnts[datectrl].nil?
+      altcnts[datectrl] = 0
+    end
+
 	  # check all requests for suitability
 	  reqs.each do |req|
-	    unless req.shifts_desired == 0 || req.assigned_shifts == max_shifts
+	    unless req.shifts_desired == 0 || req.assigned_shifts == max_shifts && !( assignedcnts[datectrl] == ConfigValue.find_by_key('max_assigned').value.to_i &&  altcnts[datectrl] == ConfigValue.find_by_key('max_alts').value.to_i)
 	      # check and see if this person is a match
-	      shift = Requestedshift.get_request(req.id,datectrl.day)
+	      shift = Requestedshift.get_request(req.id,datectrl)
 	      unless shift[0].nil?
 					# is this person requesting any for this day    
 					req.shifts_desired -= 1
 					req.assigned_shifts += 1
-					if assigned < ConfigValue.find_by_key('max_assigned').value.to_i
+					if assignedcnts[datectrl] < ConfigValue.find_by_key('max_assigned').value.to_i
 		  			shift[0].assigned_flag = 1
-		  			assigned += 1
+		  			assignedcnts[datectrl] += 1
 					else
-		  			shift[0].assigned_flag = 2
-		  			alts += 1
+						if altcnts[datectrl] < ConfigValue.find_by_key('max_alts').value.to_i
+			  			shift[0].assigned_flag = 2
+			  			altcnts[datectrl] += 1
+						else
+							# didn't actually get a shift		
+							req.shifts_desired += 1
+		          req.assigned_shifts -= 1
+						end
 					end
 					shift[0].save
-					if req.name=="limit1tuesdays"
-		  			puts shift.inspect
-					end
-					if assigned == ConfigValue.find_by_key('max_assigned').value.to_i && alts == ConfigValue.find_by_key('max_alts').value.to_i
-		  			break
-					end
 	      end
 	    end
 	  end
 	  # unclaimed tues/thurs
-    unclaimed_shifts += ConfigValue.find_by_key('max_assigned').value.to_i-assigned + ConfigValue.find_by_key('max_alts').value.to_i-alts
+    unclaimed_shifts += ConfigValue.find_by_key('max_assigned').value.to_i- assignedcnts[datectrl] + ConfigValue.find_by_key('max_alts').value.to_i-altcnts[datectrl]
 	end
 	# move to the next day in range
 	datectrl = datectrl+1
--- app/models/requestedshift.rb
+++ app/models/requestedshift.rb
@@ -1,11 +1,11 @@
 class Requestedshift < ActiveRecord::Base
   
   def self.get_request(reqid,day)
-    Requestedshift.find_by_sql(["select * from requestedshifts where request_id=? and DAY(target_date)=?",reqid,day])
+    Requestedshift.find_by_sql(["select * from requestedshifts where request_id=? and target_date=? and assigned_flag=0",reqid,day])
   end
   
   def self.get_assigned(date)
-    Requestedshift.find_by_sql(["select * from requestedshifts a inner join requests b on a.request_id = b.id where (assigned_flag =1 or assigned_flag=2) and DAY(target_date)=? and b.enrollment_id = ? order by assigned_flag",date.day.to_s,Enrollment.get_latest.id])
+    Requestedshift.find_by_sql(["select * from requestedshifts a inner join requests b on a.request_id = b.id where (assigned_flag =1 or assigned_flag=2) and target_date=? and b.enrollment_id = ? order by assigned_flag",date.to_s,Enrollment.get_latest.id])
   end
 
 end
